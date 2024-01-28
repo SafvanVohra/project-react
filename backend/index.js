10c6487cf5dc3b6344c84ad8d85f3a703f5fa2fe
sothulthorn <thornsothul@gmail.com>
2024-01-28 17:22:15 +0900

    Connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB successfully');
+  })
+  .catch((err) => console.log(err));
 
 const app = express();
 
