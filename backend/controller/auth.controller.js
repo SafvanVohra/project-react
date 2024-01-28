9361a3a102469d7c740e3900ba712fc1df12b2dc
sothulthorn <thornsothul@gmail.com>
2024-01-28 21:02:11 +0900

    Create SignIn API route

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,5 +1,7 @@
 const User = require('../models/user.model');
 const bcryptjs = require('bcryptjs');
+const errorHandler = require('../utils/error');
+const jwt = require('jsonwebtoken');
 
 exports.signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,6 +11,29 @@ exports.signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json('User created successfully!!');
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User not found.'));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credential!'));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -4,5 +4,6 @@ const authController = require('../controller/auth.controller');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
+router.get('/signin', authController.signin);
 
 module.exports = router;
